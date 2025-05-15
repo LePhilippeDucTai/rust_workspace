ad38f3cf30dc36a212e53e3f7408d324c163d0d8
Philippe LE <le.philippe.duc.tai@gmail.com>
2025-05-15 22:53:57 +0200

    Improved algorithm.

--- sudoku/src/main.rs
+++ sudoku/src/main.rs
@@ -111,15 +111,15 @@ fn solve_sudoku(board: Board) -> Result<Board, InvalidSudoku> {
     let possible_candidates = board.compute_candidates();
     println!("Possible Candidates: {:?}", possible_candidates);
     if let Ok(candidates) = possible_candidates {
-        if candidates.is_empty() {
-            return Ok(board);
-        }
-        let ((i, j), selected) = candidates.iter().min_by_key(|(_, x)| x.len()).unwrap();
-        for v in selected {
-            let new_board = board.clone().with_values(*i, *j, *v);
-            if let Ok(solution) = solve_sudoku(new_board) {
-                return Ok(solution);
+        if let Some(((i, j), selected)) = candidates.iter().min_by_key(|(_, x)| x.len()) {
+            for v in selected {
+                let new_board = board.clone().with_values(*i, *j, *v);
+                if let Ok(solution) = solve_sudoku(new_board) {
+                    return Ok(solution);
+                }
             }
+        } else {
+            return Ok(board);
         }
     }
     Err(InvalidSudoku)
